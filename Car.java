3932e7d285b4ef8511abe44dc39d8f1e6b7f299e
Pixel567 <158091258+Pixel567@users.noreply.github.com>
2024-10-16 07:52:46 +0000

    update

--- Car.java
+++ Car.java
@@ -1,31 +1,43 @@
-// include the necessary libraries
+import java.util.Objects;
 
-class Car {
-    public:
-        //attributes
-        string make;
-        string model;
-        int year;
-        // Constructor
-        Car(string make, string model, int year);
-        // Method to display car information
-        void displayInfo() { 
-            cout << "Make:" + make;
-            cout << "Model: " + model; 
-            cout << "Year: " + year;
-        }
-};
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+    private int currentYear = 2024;
 
-int main() {
-    // Create a Car object
-    Car car;
-    car.make = "Toyota";
-    car.model = "Yaris"; 
-    car.year = 2020;
+    // Constructor
+    public Car(String Make, String Model, int Year) {
+        make = Make;
+        model = Model;
+        year = Year;
     }
 
-    // Display car details
-    Car.displayInfo();
+    // Method to display car information
+    public void showInfo() {
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
 
-    return 0;
+    public void getCarAge() {
+        System.out.println("Car age: " + (currentYear - year));
+    }
+
+    public boolean isSameCar(Car otherCar) {
+        return (make.equals(otherCar.make) && model.equals(otherCar.model) && year == otherCar.year);
+    }
+
+    public static void main(String[] args) {
+        // Create a Car object
+        Car Car1 = new Car("Toyota", "Yaris", 2020);
+        Car Car2 = new Car("BMW", "XM", 2018);
+        
+        // Display car details
+        Car1.showInfo();
+        Car1.isSameCar(Car2);
+
+        // Additional call to check car age
+        Car1.getCarAge();
+    }
 }
